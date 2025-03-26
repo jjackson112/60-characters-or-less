812735085a920c586b040c4071f407d4b347194b
Jazz <jjackson11211@gmail.com>
2025-03-26 18:30:38 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
+  const maxLength = 60;
+
+  const handleOnChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccess(true);
+  };
+
+  return (
+    <div className="App">
+      {showSuccess ? (
+        <p className="message-success">Thanks for the update!</p>
+      ) : (
+        <div className="status-update">
+          <h1>Super Short Status!</h1>
+          <p>Tell us how you are doing in 60 characters or less!</p>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              className={message.length > maxLength ? "too-long" : ""}
+              placeholder="What's good?"
+              value={message}
+              onChange={handleOnChange}
+            />
+            <button
+              disabled={message.length > maxLength || message.length < 1}
+              type="submit"
+            >
+              Tell the world!
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
